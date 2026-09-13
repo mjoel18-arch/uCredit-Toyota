956080743b31fb43b3122f0deff2c860117aa0d5
Joel Felipe Mora Lugo <jmora@telepro.com.mx>
2026-09-13 02:27:19 -0600

    chore: configure frontend linting and pin dependencies

---
+++ src/uCredit.Web/eslint.config.js
@@ -0,0 +1,33 @@
+import eslint from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.flat.recommended.rules,
+      ...reactRefresh.configs.vite.rules,
+      'react-refresh/only-export-components': 'off',
+    },
+  },
+);
+
+
